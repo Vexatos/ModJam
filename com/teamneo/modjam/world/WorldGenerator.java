96032d2c43e6a02caac1c8b873111b722836bb48
Leah <asyncronous16@gmail.com>
2013-08-01 20:29:48 -0400

    Working on WorldGeneratory

---
+++ com/teamneo/modjam/world/WorldGenerator.java
@@ -0,0 +1,14 @@
+package com.teamneo.modjam.world;
+
+import java.util.Random;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import cpw.mods.fml.common.IWorldGenerator;
+
+public final class WorldGenerator implements IWorldGenerator{
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider){
+		
+	}
+}
