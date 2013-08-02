738fdc439ce9dc2f5508c301e2e648fa191e4211
Leah <asyncronous16@gmail.com>
2013-08-01 21:01:50 -0400

    Session Data And Scuba Top

--- src/com/teamneo/modjam/items/ItemScubaSuitTop.java
+++ src/com/teamneo/modjam/items/ItemScubaSuitTop.java
@@ -1,9 +1,22 @@
 package com.teamneo.modjam.items;
 
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+
+import com.teamneo.modjam.TeamNEO;
+import com.teamneo.modjam.misc.SessionData;
 
 public final class ItemScubaSuitTop extends ItemArmor{
 	public ItemScubaSuitTop(int id){
-		
+		super(id, TeamNEO.materialScuba, SessionData.RENDER_SCUBA_TOP, 2);
+		this.setCreativeTab(CreativeTabs.tabCombat);
+		this.setUnlocalizedName("teamNeoScubaTop");
+	}
+	
+	@Override
+	public String getArmorTexture(ItemStack stac, Entity entity, int slot, int layer){
+		return null;
 	}
 }
---
+++ src/com/teamneo/modjam/misc/SessionData.java
@@ -0,0 +1,7 @@
+package com.teamneo.modjam.misc;
+
+import com.teamneo.modjam.TeamNEO;
+
+public final class SessionData{
+	public static final int RENDER_SCUBA_TOP = TeamNEO.proxy.addArmor("scubaTop");
+}
