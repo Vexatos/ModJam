b530cae95eb652a3aa98fdef5cb6f67611d5ab74
Leah <asyncronous16@gmail.com>
2013-08-02 01:31:14 -0400

    Work on config

--- src/com/teamneo/modjam/misc/SessionData.java
+++ src/com/teamneo/modjam/misc/SessionData.java
@@ -8,5 +8,11 @@ public final class SessionData{
 	public static final int RENDER_SCUBA_HELMET = TeamNEO.proxy.addArmor("scubaHelmet");
 	public static final int RENDER_SCUBA_FLIPPERS = TeamNEO.proxy.addArmor("scubaFlippers");
 	
+	public static int ITEM_SCUBA_TOP_ID = 5000;
+	public static int ITEM_SCUBA_HELMET_ID = 5001;
+	public static int ITEM_SCUBA_FLIPPERS_ID = 5003;
+	public static int ITEM_SCUBA_LEGGINGS_ID = 5002;
+	public static int ITEM_PEARL = 5004;
+	
 	public static String CURRENT_LANG = "en_US";
 }
