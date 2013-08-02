991d01de4ea2583dfe83af0261bd49ecf347772e
lclc98 <liam.c14785@gmail.com>
2013-08-02 17:19:21 +1000

    Fixed NPE

--- src/com/teamneo/modjam/blocks/Blocks.java
+++ src/com/teamneo/modjam/blocks/Blocks.java
@@ -1,15 +1,12 @@
 package com.teamneo.modjam.blocks;
 
-<<<<<<< HEAD
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
-=======
->>>>>>> c495ae4fe4174ee1881f2ecad8baa5053df92fed
 
-public final class Blocks{
-	public static final Block blockClam = new BlockClam(3000);
-	
-	public static void addBlocks(){
+public final class Blocks {
+	public static Block blockClam = new BlockClam(3000);
+
+	public static void addBlocks() {
 		GameRegistry.registerBlock(blockClam, "teamNeoBlockClam");
 	}
 }
